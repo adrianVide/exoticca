7ec7e3179c9f5e98504514f938016559a90d4123
adrian <adrian.vide.paris@gmail.com>
2024-02-27 21:24:58 +0100

    Add scroll functionality and logo hiding in SearchBar component

--- app/components/SearchBar/SearchBar.tsx
+++ app/components/SearchBar/SearchBar.tsx
@@ -1,29 +1,50 @@
+import { useState, useEffect } from 'react';
 import styles from './searchbar.module.css';
+import Image from 'next/image';
 
 type SearchBarProps = {
     handleChange: (searchTerm: string) => void
     searchTerm: string
+    canHide?: boolean
 }
-const SearchBar = ({ handleChange, searchTerm }: SearchBarProps) => {
+const SearchBar = ({ handleChange, searchTerm, canHide = true }: SearchBarProps) => {
+    const [isAtTop, setIsAtTop] = useState(true);
+    console.log('isAtTop', isAtTop)
+    useEffect(() => {
+        function handleScroll() {
+            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            setIsAtTop(scrollTop === 0);
+        }
+
+        // Listen to scroll events
+        window.addEventListener('scroll', handleScroll);
+
+        // Clean up the event listener
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div className={styles.searchbar__wrapper}>
             <div className={styles.searchbar__container}>
-                <div className={styles.searchbar__icon}>
+                {isAtTop && canHide ? <Image alt='logo' className={styles.searchbar__logo} width={275} height={48} src='/logo.webp'></Image> : <><div className={styles.searchbar__icon}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="1.3em" height="1.3em" data-testid="search-icon">
                         <title>search-icon</title>
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.05} d="M11 19.004a8 8 0 1 0 0-16 8 8 0 0 0 0 16M21 21.002l-4.35-4.35"></path>
                     </svg>
                 </div>
-                <input
-                    type="text"
-                    placeholder="Search your destination"
-                    value={searchTerm}
-                    onChange={e => handleChange(e.target.value)}
-                    className={styles.searchbar__input}
-                />
-                <button type="button" className={styles.search__button} >
-                    Search
-                </button>
+                    <input
+                        type="text"
+                        placeholder="Search your destination"
+                        value={searchTerm}
+                        onChange={e => handleChange(e.target.value)}
+                        className={styles.searchbar__input}
+                    />
+                    <button type="button" className={styles.search__button} >
+                        Search
+                    </button></>}
+
             </div>
         </div>
     )
