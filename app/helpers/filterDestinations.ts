76e6d08c2c34ddb3df990c0f82088d07b9031f98
adrian <adrian.vide.paris@gmail.com>
2024-02-28 18:30:55 +0100

    Refactor filterDestinations function to use TypeScript types

--- app/helpers/filterDestinations.ts
+++ app/helpers/filterDestinations.ts
@@ -1,5 +1,13 @@
- // Recursive function to check if any value in the object or its nested objects matches the search term
- const containsTerm = (obj: any, term: string | number): boolean => {
+import { destination } from '../types/types';
+
+type destinations = {
+    featuredMonoMarket: destination[];
+    featuredMultiMarket: destination[];
+    monoMarket: destination[];
+    multiMarket: destination[];
+}
+
+const containsTerm = (obj: any, term: string | number): boolean => {
     for (const key in obj) {
         if (typeof obj[key] === 'object' && obj[key] !== null) {
             if (containsTerm(obj[key], term)) {
@@ -14,18 +22,15 @@
     return false;
 };
 
-// Function to filter destinations based on the search term
-export const filterDestinations = (term: string, destinations) => {
-    // Determine if the term is a number or a string
+export const filterDestinations = (term: string, destinations: destinations) => {
     const parsedTerm = isNaN(Number(term)) ? term : Number(term);
 
-    // Concatenate all destination arrays into one array
     const allDestinations = destinations.featuredMonoMarket
         .concat(destinations.featuredMultiMarket)
         .concat(destinations.monoMarket)
         .concat(destinations.multiMarket);
 
-    return allDestinations.filter((destination: any) => {
+    return allDestinations.filter((destination: destination) => {
         return containsTerm(destination, parsedTerm);
     });
 };
