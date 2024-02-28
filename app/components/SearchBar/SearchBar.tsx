2c36f7642aba048dc6e185ed8de2fecda136ae4d
adrian <adrian.vide.paris@gmail.com>
2024-02-28 18:31:42 +0100

    Refactor SearchBar component to use canHideSearch prop

--- app/components/SearchBar/SearchBar.tsx
+++ app/components/SearchBar/SearchBar.tsx
@@ -5,21 +5,16 @@ import Image from 'next/image';
 type SearchBarProps = {
     handleChange: (searchTerm: string) => void
     searchTerm: string
-    canHide?: boolean
+    canHideSearch?: boolean
 }
-const SearchBar = ({ handleChange, searchTerm, canHide = true }: SearchBarProps) => {
+const SearchBar = ({ handleChange, searchTerm, canHideSearch = true }: SearchBarProps) => {
     const [isAtTop, setIsAtTop] = useState(true);
-    console.log('isAtTop', isAtTop)
     useEffect(() => {
         function handleScroll() {
-            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            const scrollTop = window.scrollY || document.documentElement.scrollTop;
             setIsAtTop(scrollTop === 0);
         }
-
-        // Listen to scroll events
         window.addEventListener('scroll', handleScroll);
-
-        // Clean up the event listener
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
@@ -28,23 +23,26 @@ const SearchBar = ({ handleChange, searchTerm, canHide = true }: SearchBarProps)
     return (
         <div className={styles.searchbar__wrapper}>
             <div className={styles.searchbar__container}>
-                {isAtTop && canHide ? <Image alt='logo' className={styles.searchbar__logo} width={275} height={48} src='/logo.webp'></Image> : <><div className={styles.searchbar__icon}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="1.3em" height="1.3em" data-testid="search-icon">
-                        <title>search-icon</title>
-                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.05} d="M11 19.004a8 8 0 1 0 0-16 8 8 0 0 0 0 16M21 21.002l-4.35-4.35"></path>
-                    </svg>
-                </div>
-                    <input
-                        type="text"
-                        placeholder="Search your destination"
-                        value={searchTerm}
-                        onChange={e => handleChange(e.target.value)}
-                        className={styles.searchbar__input}
-                    />
-                    <button type="button" className={styles.search__button} >
-                        Search
-                    </button></>}
-
+                {isAtTop && canHideSearch
+                    ? <Image alt='logo' className={styles.searchbar__logo} width={275} height={48} src='/logo.webp'></Image>
+                    :
+                    <><div className={styles.searchbar__icon}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" width="1.3em" height="1.3em" data-testid="search-icon">
+                            <title>search-icon</title>
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.05} d="M11 19.004a8 8 0 1 0 0-16 8 8 0 0 0 0 16M21 21.002l-4.35-4.35"></path>
+                        </svg>
+                    </div>
+                        <input
+                            type="text"
+                            placeholder="Search your destination"
+                            value={searchTerm}
+                            onChange={e => handleChange(e.target.value)}
+                            className={styles.searchbar__input}
+                        />
+                        <button type="button" className={styles.search__button} >
+                            Search
+                        </button>
+                    </>}
             </div>
         </div>
     )
